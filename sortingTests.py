f2fc56168abde0db58bf9bdcf1daacdc29bf84e9
pbraunstein <pbraunstein12@gmail.com>
2017-10-29 15:40:00 -0400

    better test runner

--- sortingTests.py
+++ sortingTests.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
 
 import subprocess
-import unittest
 
-class sortingTests(unittest.TestCase):
-    pass
+from sys import argv
 
 def main():
-    proc = subprocess.Popen(['./isort', '8', '2', '3', '4'], stdout=subprocess.PIPE)
+    test_already_sorted()
+
+def test_already_sorted():
+    print "test already sorted.....",
+    _run_test([2, 3, 4, 8], '2 3 4 8')
+    print "successs"
+
+def _run_test(testInput, expectedOutput):
+    testInput = [str(x) for x in testInput]
+    proc = subprocess.Popen([argv[1], '2', '3', '4', '8'], stdout=subprocess.PIPE)
+    proc = subprocess.Popen([argv[1]] + testInput, stdout=subprocess.PIPE)
     stdout, _ = proc.communicate()
-    assert stdout.strip() == '2 3 4 8'
+    assert stdout.strip() == expectedOutput
 
 main()
 
